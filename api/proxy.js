9bbbe82fd47bc7138aabfb5fbe4e8c7ac73568cc
rockynh <rockyxie@hotmail.com>
2023-06-15 21:31:22 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -19,37 +19,6 @@ module.exports = (req, res) => {
       //  /backend/user/login => http://google.com/user/login
       //   "^/backend/": "/",
     },
-        selfHandleResponse: true,
-    onProxyRes (proxyRes, req, res) {
-        const bodyChunks = [];
-        proxyRes.on('data', (chunk) => {
-            bodyChunks.push(chunk);
-        });
-        proxyRes.on('end', () => {
-            const body = Buffer.concat(bodyChunks);
 
-            // forwarding source status
-            res.status(proxyRes.statusCode);
-
-            // forwarding source headers
-            Object.keys(proxyRes.headers).forEach((key) => {
-                res.append(key, proxyRes.headers[key]);
-            });
-
-            // modifying html content
-            if (proxyRes.headers['content-type'] && proxyRes.headers['content-type'].includes('text/html')) {
-                let html = body.toString();
-
-                // do whatever you want
-                html = html.replace('search.yahoo.co.jp', 'y1.michx.cf');
-
-                res.send(new Buffer.from(html));
-            } else {
-                res.send(body);
-            }
-
-            res.end();
-        });
-    },
   })(req, res);
 };
