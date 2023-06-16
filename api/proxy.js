c5a9937793e82e5e2201023ab0f907c50697c35a
rockynh <rockyxie@hotmail.com>
2023-06-16 09:22:47 +0800

    retest for the yahoo

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware, responseInterceptor } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
   let target = "https://search.yahoo.co.jp/";//your website url
@@ -19,6 +19,25 @@ module.exports = (req, res) => {
       //  /backend/user/login => http://google.com/user/login
       //   "^/backend/": "/",
     },
+  selfHandleResponse: true, // manually call res.end(); IMPORTANT: res.end() is called internally by responseInterceptor()
+  on: {
+    proxyRes: responseInterceptor(async (buffer, proxyRes, req, res) => {
+      // log original response
+      // console.log(`[DEBUG] original response:\n${buffer.toString('utf8')}`);
+      const response = buffer.toString('utf8');
+        // modifying html content
+        if (proxyRes.headers['content-type'] && proxyRes.headers['content-type'].includes('text/html')) {
+            
+
+            // do whatever you want
+            // html = response.replace('search.yahoo.co.jp', 'y1.michx.cf');
+            return response.replaceAll('search.yahoo.co.jp', 'y1.michx.cf');
+      
+        } else {
+          return response;
+        }
+    }),
+  },
 
   })(req, res);
 };
