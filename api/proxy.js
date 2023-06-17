00217e8c0202d9290507bc6eb4ee5cd0782b06c0
rockynh <rockyxie@hotmail.com>
2023-06-17 08:52:20 +0800

    Update proxy.js1080

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware , responseInterceptor} = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://y1.michx.cf/";
+  let target = "https://search.yahoo.co.jp/";
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   //   if (
@@ -22,6 +22,31 @@ module.exports = (req, res) => {
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
       //   "^/backend/": "/",
     },
+  selfHandleResponse: true, // res.end() will be called internally by responseInterceptor()
 
+  logger: console,
+
+  /**
+   * Intercept response and replace 'Hello' with 'Goodbye'
+   **/
+  on: {
+    proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+      const hostname = req.headers.host;
+      //console.log(`HOST= ${hostname}`);
+      const response = responseBuffer.toString('utf8'); // convert buffer to string
+
+      let url = req.url;
+      console.log(`ReqURL= ${url}`);
+
+     let ct = proxyRes.headers['content-type'];
+     // console.log(`ContentType= ${ct}`);
+      // modifying html content
+      if (proxyRes.headers['content-type'] && proxyRes.headers['content-type'].includes('text/html')) {
+        console.log('begin replace host');  
+        return response.replaceAll('search.yahoo.co.jp', hostname); // manipulate response and return the result
+      }
+      return response
+    }),
+  },
   })(req, res);
 };
