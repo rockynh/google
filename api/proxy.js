e1b2e809ab94ed701148f3c72b6b1f69f6ddb859
rockynh <rockyxie@hotmail.com>
2023-06-17 08:13:27 +0800

    Update proxy.js 1050

--- api/proxy.js
+++ api/proxy.js
@@ -22,24 +22,6 @@ module.exports = (req, res) => {
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
       //   "^/backend/": "/",
     },
-    selfHandleResponse: true, // res.end() will be called internally by responseInterceptor()
-    logger: console,
-    /**
-     * Intercept response and replace 'Hello' with 'Goodbye'
-     **/
-    on: {
-      proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-        const hostname = req.headers.host;
-        const response = responseBuffer.toString('utf8'); // convert buffer to string
-        let ct = proxyRes.headers['content-type'];
-        //console.log(`ContentType= ${ct}`);
-        // modifying html content
-        if (proxyRes.headers['content-type'] && proxyRes.headers['content-type'].includes('text/html')) {
-          console.log('begin replace host');  
-          return response.replaceAll('search.yahoo.co.jp', hostname); // manipulate response and return the result
-        }
-        return response
-    }),
-  },
+
   })(req, res);
 };
